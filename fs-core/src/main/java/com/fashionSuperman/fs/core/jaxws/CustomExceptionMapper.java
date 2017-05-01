425875eda3e3ef9f11803af764999ecd15b300cf
FashionSuperman <zhruixuan@163.com>
2017-05-02 00:14:11 +0800

    ggg

--- fs-core/src/main/java/com/fashionSuperman/fs/core/jaxws/CustomExceptionMapper.java
+++ fs-core/src/main/java/com/fashionSuperman/fs/core/jaxws/CustomExceptionMapper.java
@@ -3,6 +3,9 @@ package com.fashionSuperman.fs.core.jaxws;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fashionSuperman.fs.core.constant.StatusCode;
 import com.fashionSuperman.fs.core.exception.BizException;
 import com.fashionSuperman.fs.core.mvc.entity.ResponseMessage;
@@ -16,15 +19,23 @@ import com.fashionSuperman.fs.core.util.StringUtil;
  * @date 2017年3月9日 下午1:53:26
  * @version 1.0
  */
-public class CustomExceptionMapper implements ExceptionMapper<Throwable> {
+public class CustomExceptionMapper implements ExceptionMapper<Exception> {
+	private Logger logger = LoggerFactory.getLogger(CustomExceptionMapper.class);
 
 	@Override
-	public Response toResponse(Throwable exception) {
+	public Response toResponse(Exception exception) {
 		ResponseMessage<Object> responseMessage = new ResponseMessage<>();
 		String message = "";
 		String code = StatusCode.FAILURE;
 		if (exception instanceof BizException) {
 			BizException bizException = (BizException) exception;
+			message = bizException.getMessage();
+			int intCode = bizException.getCode();
+			if(intCode != 0){
+				code = String.valueOf(intCode);
+			}
+		} else if(exception.getCause() instanceof BizException){
+			BizException bizException = (BizException) exception.getCause();
 			message = bizException.getMessage();
 			int intCode = bizException.getCode();
 			if(intCode != 0){
@@ -41,7 +52,8 @@ public class CustomExceptionMapper implements ExceptionMapper<Throwable> {
 		responseMessage.setMessage(message);
 		responseMessage.setResponseData("");
 		responseMessage.setCode(code);
-
+		
+		logger.error(message);
 
 		return Response.status(Response.Status.NOT_FOUND).entity(responseMessage)
 				.type("application/json").build();
---
+++ fs-core/src/main/java/com/fashionSuperman/fs/core/jaxws/OpetionsMethodFilter.java
@@ -0,0 +1,28 @@
+package com.fashionSuperman.fs.core.jaxws;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.core.Headers;
+import org.jboss.resteasy.specimpl.BuiltResponse;
+
+public class OpetionsMethodFilter implements ContainerRequestFilter{
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		if(requestContext.getMethod().equals("OPTIONS")){
+			Response response = new BuiltResponse();
+			response.status(204);
+			Headers<String> metadata = new Headers<>();
+			metadata.add("Access-Control-Allow-Method", "GET,POST");
+			metadata.add("Access-Control-Allow-Origin", "*");
+			metadata.add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+			requestContext.abortWith(response);
+		}
+		
+	}
+
+}
