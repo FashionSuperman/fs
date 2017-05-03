cd9d840137df27740aee0eb8292ae4c74362843a
1462989455@qq <1462989455@qq>
2017-05-03 08:36:13 +0800

    g

--- fs-core/src/main/java/com/fashionSuperman/fs/core/jaxws/OpetionsMethodFilter.java
+++ fs-core/src/main/java/com/fashionSuperman/fs/core/jaxws/OpetionsMethodFilter.java
@@ -6,7 +6,6 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 
-import org.jboss.resteasy.core.Headers;
 import org.jboss.resteasy.specimpl.BuiltResponse;
 
 public class OpetionsMethodFilter implements ContainerRequestFilter{
@@ -15,11 +14,17 @@ public class OpetionsMethodFilter implements ContainerRequestFilter{
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		if(requestContext.getMethod().equals("OPTIONS")){
 			Response response = new BuiltResponse();
-			response.status(204);
-			Headers<String> metadata = new Headers<>();
-			metadata.add("Access-Control-Allow-Method", "GET,POST");
-			metadata.add("Access-Control-Allow-Origin", "*");
-			metadata.add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+			response = Response.status(204)
+					.header("Access-Control-Allow-Method", "GET,POST")
+					.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+					.entity("")
+					.build();
+//			response.status(204);
+//			Headers<String> metadata = new Headers<>();
+//			metadata.add("Access-Control-Allow-Method", "GET,POST");
+//			metadata.add("Access-Control-Allow-Origin", "*");
+//			metadata.add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 			requestContext.abortWith(response);
 		}
 		
